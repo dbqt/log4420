ca2be15591364beee851d2006cb40a00b410dee8
chtref <charles-2.tremblay@polymtl.ca>
2016-09-24 13:35:32 -0400

    Debut de styles, la navbar semble fonctionnelle. Basically, blocks are placeholders in parents that will be defined in children. I use a block to determine what is the selected page

--- web_tp2/webtp2/app.js
+++ web_tp2/webtp2/app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var question = require('./routes/question');
-var instruction = require('./routes/instruction');
+var instructions = require('./routes/instructions');
 var dashboard = require('./routes/dashboard')
 
 var app = express();
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/instruction', instruction);
+app.use('/instructions', instructions);
 app.use('/question', question);
 app.use('/dashboard', dashboard);
 
--- web_tp2/webtp2/routes/instructions.js
+++ web_tp2/webtp2/routes/instructions.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.render('instruction', { title: 'Intructions' });
+  res.render('instructions', { title: 'Intructions' });
 });
 
 module.exports = router;
