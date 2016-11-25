24f752de46738bd300e2da25e9b1c989155d618e
datraa <david.tran@polymtl.ca>
2016-11-25 18:22:54 -0500

    api up and running

---
+++ web_tp5/lib/db.js
@@ -0,0 +1,57 @@
+var mongoose = require( 'mongoose' );
+var random = require('mongoose-simple-random');
+
+var Schema = mongoose.Schema;
+
+var Questions = new Schema({
+ domaine : String,
+ question : String,
+ reponse1 : String,
+ reponse2 : String,
+ reponse3 : String,
+ answer : String
+});
+Questions.plugin(random);
+
+var Stats = new Schema({
+    testRapide : {
+        reussi : Number,
+        echoue : Number
+    },
+    examen : {
+        reussi : {
+            HTML : Number,
+            JavaScript : Number,
+            CSS : Number
+        },
+        echoue : {
+            HTML : Number,
+            JavaScript : Number,
+            CSS : Number
+        }
+    },
+    examensDetailles : [
+        {
+            nom: String,
+            domaine : String,
+            score : Number,
+            nbQuestions : Number
+        }
+    ],
+    progres : {
+        examenEnCours: Boolean,
+        domaineEnCours : String,
+        scoreEnCours : Number,
+        nbQuestionsEnCours : Number,
+        numeroQuestionEnCours : Number,
+        scoreTestRapide : Number,
+        numeroQuestionTestRapide : Number,
+    },
+    mode : String
+});
+
+mongoose.model( 'Questions', Questions );
+mongoose.model( 'Stats', Stats );
+// user: 'epicUser' 
+// password: 'epicpassword1'
+mongoose.connect( 'mongodb://epicUser:epicpassword1@ds050189.mlab.com:50189/tpweb' );
